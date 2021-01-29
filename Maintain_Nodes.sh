571f32766bc09b94e3ee2031ec560ddaf3a8de9f
Yaclaudes Minoso <50747740+yaclaudes1@users.noreply.github.com>
2021-01-29 01:33:40 -0500

    Update Maintain_Nodes.sh

--- Maintain_Nodes.sh
+++ Maintain_Nodes.sh
@@ -1,8 +1,8 @@
 ##This script is currently set to be run strictly in the NENG Directory where ./nengcoind is located at
-##While loop on a case by case
-##Pseudo Code: While(Ctrl-C not entered)
-##					Wait 24 Hours to send all coins to master node. 
-##					
+##
+##					Line 11 causes the script to wait for a certain time in seconds
+##					3600 seconds = 1 hour; Wait 1 Hour to send all coins to master node. 
+##					Line 3 
 ##					
 ##					
 while :
@@ -11,6 +11,6 @@ do
 	sleep 3600
 	read -r -d '' BALANCE_AMOUNT < <(./nengcoind getbalance)
 	echo "$BALANCE_AMOUNT"
-	./nengcoind sendtoaddress NfmS7YSgFuaMtcZpFrPaJRwu7L1ExXfX3Z "$BALANCE_AMOUNT"
+	./nengcoind sendtoaddress //WALLETADDRESSGOESHERE!// "$BALANCE_AMOUNT"
 done
 exit 0
