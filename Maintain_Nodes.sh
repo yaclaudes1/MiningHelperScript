d3c73bbd8d29f9d713ccf71122d47d4761567421
Yaclaudes Minoso <50747740+yaclaudes1@users.noreply.github.com>
2021-01-29 01:36:29 -0500

    Add files via upload

--- Maintain_Nodes.sh
+++ Maintain_Nodes.sh
@@ -1,16 +1,20 @@
 ##This script is currently set to be run strictly in the NENG Directory where ./nengcoind is located at
-##
-##					Line 11 causes the script to wait for a certain time in seconds
-##					3600 seconds = 1 hour; Wait 1 Hour to send all coins to master node. 
-##					Line 3 
+##While loop on a case by case
+##Pseudo Code: While(Ctrl-C not entered)
+##					 
+##			SLEEP SET TO 4 HOURS		
 ##					
-##					
+##	
+WALLET_ADDRESS=NQFAb97o624vYuTWg6oEqYKp74Ldnei7Ak 				
 while :
 do
 	echo "To quit press Ctrl-C"
-	sleep 3600
+	sleep 14400
 	read -r -d '' BALANCE_AMOUNT < <(./nengcoind getbalance)
-	echo "$BALANCE_AMOUNT"
-	./nengcoind sendtoaddress //WALLETADDRESSGOESHERE!// "$BALANCE_AMOUNT"
+	echo "Balance Amount Pre-Transfer: $BALANCE_AMOUNT"
+	read -r -d '' BALANCE_MINUS_FEES < <(bc <<< "$BALANCE_AMOUNT * .7")
+	./nengcoind sendtoaddress "$WALLET_ADDRESS" "$BALANCE_MINUS_FEES"
+	date +"Time Statistics: %m/%d/%Y Time: %H.%M.%S"
+	echo "Balance Amount Post-transfer: $BALANCE_AMOUNT"
 done
 exit 0
